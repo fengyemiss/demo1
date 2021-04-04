26ff36b8f7c1f06f76c6bcd849d68ee25a5d739c
yikang <656379608@qq.com>
2021-04-04 20:34:18 +0800

    提交代码

---
+++ gitText/src/com/offcn/GitText.java
@@ -0,0 +1,10 @@
+package com.offcn;
+
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
